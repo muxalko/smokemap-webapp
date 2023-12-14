f33f879b8c2b017f555262f08c06cba9b24d4e24
Muxalko <muxa.baba@gmail.com>
2023-12-14 22:43:52 +0200

    correct configs for typescript in project

--- next.config.js
+++ next.config.js
@@ -1,31 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-  compiler: {
-    // ssr and displayName are configured by default
-   styledComponents: true,
-  //   // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-  //   styledComponents: boolean | {
-  //       // Enabled by default in development, disabled in production to reduce file size,
-  //       // setting this will override the default for all environments.
-  //       displayName?: boolean,
-  //       // Enabled by default.
-  //       ssr?: boolean,
-  //       // Enabled by default.
-  //       fileName?: boolean,
-  //       // Empty by default.
-  //       topLevelImportPaths?: string[],
-  //       // Defaults to ["index"].
-  //       meaninglessFileNames?: string[],
-  //       // Enabled by default.
-  //       cssProp?: boolean,
-  //       // Empty by default.
-  //       namespace?: string,
-  //       // Not supported yet.
-  //       minify?: boolean,
-  //       // Not supported yet.
-  //       transpileTemplateLiterals?: boolean,
-  //       // Not supported yet.
-  //       pure?: boolean,
-  //     },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+    ],
   },
-}
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
