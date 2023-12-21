339edcdff0d76c92b8c9309a72c9d910dc93a4f7
Muxalko <muxa.baba@gmail.com>
2023-12-21 23:24:15 +0200

    add footer

--- src/components/template/footer.tsx
+++ src/components/template/footer.tsx
@@ -3,13 +3,6 @@ export default function Footer() {
   const currentYear = d.getFullYear();
   return (
     <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © {currentYear}{" "}
-        <a href="https://flowbite.com/" className="hover:underline">
-          DaMi™
-        </a>
-        . All Rights Reserved.
-      </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a href="/about" className="hover:underline me-4 md:me-6">
@@ -22,6 +15,13 @@ export default function Footer() {
           </a>
         </li>
       </ul>
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © {currentYear}{" "}
+        <a href="https://flowbite.com/" className="hover:underline">
+          DaMi™
+        </a>
+        . All Rights Reserved.
+      </span>
     </footer>
   );
 }
