c24e7f74a9f4d3f58a68c4d3105fa3c4e20bb617
Muxalko <muxa.baba@gmail.com>
2023-12-23 00:48:16 +0200

    closes #7

--- src/components/template/navigation.tsx
+++ src/components/template/navigation.tsx
@@ -15,8 +15,8 @@ const navigation = [
   // { name: "Dashboard", href: "#", current: true },
   // { name: "Team", href: "#", current: false },
   // { name: "Projects", href: "#", current: false },
-  { name: "About", href: "/about", current: false },
-  { name: "Privacy", href: "/privacy", current: false },
+  //   { name: "About", href: "/about", current: false },
+  //   { name: "Privacy", href: "/privacy", current: false },
   { name: "Requests", href: "/requests", current: false },
 ];
 const userNavigation = [
@@ -48,21 +48,22 @@ export default function Navigation({ user }: { user: User }) {
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        aria-current={item.current ? "page" : undefined}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        href={item.href}
-                        key={item.name}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                    {user.role == "admin" &&
+                      navigation.map((item) => (
+                        <a
+                          aria-current={item.current ? "page" : undefined}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "rounded-md px-3 py-2 text-sm font-medium"
+                          )}
+                          href={item.href}
+                          key={item.name}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
                   </div>
                 </div>
               </div>
