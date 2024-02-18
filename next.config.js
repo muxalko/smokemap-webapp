83cd4f265e60c66e82d0e5b981e603513fe0354e
Muxalko <muxa.baba@gmail.com>
2024-02-18 09:08:11 +0200

    allow google images

--- next.config.js
+++ next.config.js
@@ -15,17 +15,24 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
-
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   experimental: {
     serverActions: true,
+    serverComponentsExternalPackages: ['pino'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: false
-  }
+  },
+  reactStrictMode: false
 };
 
 module.exports = nextConfig
